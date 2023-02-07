6a46b3ffb4362647b1d475c7e0dba42a406285b2
ni1212 <120764488+ni1212@users.noreply.github.com>
2023-02-07 17:03:46 +0900

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿
+var text = " a fgfd df ";
+Console.WriteLine($"{AR.Emp2(text, 2)}:");
+Console.WriteLine(AR.EmpCount(text,2));
+var chars=new char[text.Length];
+for (int i = 0;i< chars.Length; i++)
+{
+    chars[i] = text[i];
+}
+
+
+ChangeSpace(chars);
+static void ChangeSpace(params char[] chars)
+{
+    for (int i = 0; i < chars.Length; i++)
+    {
+        if (chars[i] == ' ')
+        {
+            ShiftArray(chars, i, 2);
+            chars[i++] = '%';
+            chars[i++] = '2';
+            chars[i] = '0';     // ここはi++ではない。
+        }
+    }
+    foreach (var item in chars)
+    {
+        Console.Write(item);
+    }
+}
+static void ShiftArray(char[] chars, int start, int count)
+{
+    for (int i = chars.Length - 1 - count; start <= i; i--)
+    {
+        chars[i + count] = chars[i];
+    }
+}
+
+class AR
+{
+    public static string Emp(string text,int count) => text.Replace(" ", "%20");
+
+
+    public static string AddEmp(string str,int cou)
+    {
+        for (int i = 0; i < cou; i++)
+        {
+            str += " ";
+        }
+        return str;
+    }
+    public static string Emp2(string text,int count)
+    {
+        string str = null;
+        foreach (var c in text)
+        {
+            if (c == ' ')
+            {
+                str += "%20";
+            }
+            else
+            {
+                str += c;
+            }
+            
+        }
+
+        return AddEmp(str, count);
+    }
+    public static int EmpCount(string text,int cou)
+    {
+        int count = 0;
+
+        foreach (var c in Emp2(text,cou))
+        {
+            if(char.IsWhiteSpace(c))count++;
+        }
+        return Emp2(text,cou).Length - count;
+    }
+}
---
+++ TaxCal/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCal
+{
+    public partial class Form1 : Form
+    {
+        //                              a%20s%20ssd%20s
+        public string s { get; set; } = "a s ssd s";
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Size = new Size(400, 100);
+            textBox1.Name = "PriceBox";
+
+            textBox2.Size = new Size(400, 80);
+            textBox2.Name = "taxPriceBox";
+            textBox2.Enabled= false;
+
+            button1.Name = "calcButton";
+            button1.Text = "計算する";
+
+            label1.Text = "税込価格";
+            label2.Text = "税抜価格";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = AR.Emp2(textBox1.Text,2);
+            decimal price;
+            var success=decimal.TryParse(textBox1.Text, out price);
+
+            if (success)
+            {
+                decimal tax = price * (decimal)1.1;
+                textBox2.Text=tax.ToString();
+            }
+        }
+        
+    }
+    class AR
+    {
+        public static string Emp(string text, int count) => text.Replace(" ", "%20");
+
+
+        public static string AddEmp(string str, int cou)
+        {
+            for (int i = 0; i < cou; i++)
+            {
+                str += " ";
+            }
+            return str;
+        }
+        public static string Emp2(string text, int count)
+        {
+            string str = null;
+            foreach (var c in text)
+            {
+                if (c == ' ')
+                {
+                    str += "%20";
+                }
+                else
+                {
+                    str += c;
+                }
+
+            }
+
+            return AddEmp(str, count);
+        }
+        public static int EmpCount(string text, int cou)
+        {
+            int count = 0;
+
+            foreach (var c in Emp2(text, cou))
+            {
+                if (char.IsWhiteSpace(c)) count++;
+            }
+            return Emp2(text, cou).Length - count;
+        }
+    }
+}
